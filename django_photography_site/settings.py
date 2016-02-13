98a7d78bc3d9f11492e3cfe9853eca6091af60b8
muhilvarnan V <muhilvarnan.v@gmail.com>
2016-02-14 01:43:10 +0530

    settings update files

--- django_photography_site/settings.py
+++ django_photography_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
+    'photos'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'django_photography_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
+
+THUMBNAIL_DEBUG = True
+
+
+#Site settings
+WEBSITE_TITLE = 'Muhilvarnan Photography'
+BASE_BACKGROUND_IMAGE_STATIC_PATH = '/static/photos/img/bg.jpg'
+
